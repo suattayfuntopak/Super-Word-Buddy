c38accf81b7247d127afe99602295f5e81e158fb
Suat Tayfun TOPAK <suattayfuntopak@gmail.com>
2026-03-28 08:10:36 +0300

    Update supabaseClient.ts

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,7 +1,6 @@
-
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://yayrzhqzxdicsricskce.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlheXJ6aHF6eGRpY3NyaWNza2NlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4OTE5OTEsImV4cCI6MjA4MzQ2Nzk5MX0.1uHv6_rYTxqMdn8af7mLcAhHW_0qO8c8M06PaoxaB2k';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
